8173dcc4407d87a85ceb934bdb7b51418c40e847
Troy Christian <apostasy8@gmail.com>
2020-01-24 14:15:29 -0500

    UITesting nearly done

--- Message BoardUITests/Message_BoardUITests.swift
+++ Message BoardUITests/Message_BoardUITests.swift
@@ -23,4 +23,50 @@ class Message_BoardUITests: XCTestCase {
         app.launch()
     }
     
+    func testTableViewThreadCreation() {
+        let textField = app.textFields["MessageThreadsTableViewController.TextField"]
+        //XCTAssert(textField.exists)
+        textField.tap()
+        textField.typeText("SwiftR0x")
+        app.keyboards.buttons["Return"].tap()
+        
+        let newTableEntry = app.tables.staticTexts["SwiftR0x"]
+        XCTAssertTrue(newTableEntry.exists)
+    }
+    
+    func testCreateAMessage() {
+        let textField = app.textFields["MessageThreadsTableViewController.TextField"]
+        //XCTAssert(textField.exists)
+        textField.tap()
+        textField.typeText("SwiftR0x")
+        app.keyboards.buttons["Return"].tap()
+
+        let newTableEntry = app.tables.staticTexts["SwiftR0x"]
+        XCTAssertTrue(newTableEntry.exists)
+
+        newTableEntry.tap()
+        
+//        app.navigationBars["SwiftR0x"].buttons["MessageThreadDetailTableViewController.AddNewMessageButton"].tap()
+        
+        let createNewMessage = app.navigationBars["SwiftR0x"].buttons["MessageThreadDetailTableViewController.AddNewMessageButton"]
+        
+        createNewMessage.tap()
+        
+        
+        
+        XCTAssert(app.navigationBars["New Message"].exists)
+        
+        let messageDetailTextField = app.textFields["MessageDetailViewController.TextField"]
+        XCTAssert(messageDetailTextField.exists)
+        
+        let messageDetailTextView = app.textViews["MessageDetailViewController.TextView"]
+        XCTAssert(messageDetailTextView.exists)
+        
+        let addButton = app.navigationBars["New Message"].buttons["Add"]
+        XCTAssert(addButton.exists)
+        
+        addButton.tap()
+
+    }
+    
 }
